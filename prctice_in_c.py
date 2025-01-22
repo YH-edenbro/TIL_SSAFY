6bc3b0e36498708b5cd32d5ca0ca60c59ec9dce0
YH-edenbro <hyun96227@gmail.com>
2025-01-22 09:12:05 +0900

    아침공부

--- prctice_in_c.py
+++ prctice_in_c.py
@@ -1,6 +1,14 @@
-n, m = map(int, input().split()) # 공과 바구니의 수 : n 바구니에 공을 넣는 시도 횟수 : m
+n, m = map(int, input().split()) # 공과 바구니의 수 : n / 바구니에 공을 넣는 시도 횟수 : m
 b = [] # 빈 바구니 
 
-for i in range(m) :
+for item in range(n) :
     b.append(0)
-        i, j, k = map(int, input().split())
+
+for a in range(m) :
+    i, j, k = map(int, input().split()) # i-j번 바구니까지 담기. k번 번호의 공 선정
+    c = [] # 빈 바구니에 넣을 공 주머니 리셋셋
+    for item in range(m) :
+        c.append(k)
+    b[i:j+1] = c
+
+print(b)
