8e8857665004fcfe4ceaa93710849c1edbbfd8e7
YH-edenbro <hyun96227@gmail.com>
2025-01-27 16:13:14 +0900

    한문제추가

--- Baekjoon.py
+++ Baekjoon.py
@@ -339,3 +339,23 @@ while True:
         print(string)
     except EOFError:
         break
+
+# 백준 1157번 문제 단어 공부 - 중복된 값을 어떻게 처리할지 고민하는 문제.
+
+string = list(input().upper())
+string.sort()
+
+unique_list   = []
+count_list = []
+
+for char in string:
+    if char not in unique_list:
+        unique_list.append(char)
+
+for char in unique_list:
+    count_list.append(string.count(char))
+
+if count_list.count(max(count_list)) >= 2:
+    print('?')
+else:
+    print(unique_list[count_list.index(max(count_list))])
--- prctice_in_c.py
+++ prctice_in_c.py
@@ -1,6 +1,17 @@
-while True:
-    try:
-        string = input()
-        print(string)
-    except EOFError:
-        break
+string = list(input().upper())
+string.sort()
+
+unique_list   = []
+count_list = []
+
+for char in string:
+    if char not in unique_list:
+        unique_list.append(char)
+
+for char in unique_list:
+    count_list.append(string.count(char))
+
+if count_list.count(max(count_list)) >= 2:
+    print('?')
+else:
+    print(unique_list[count_list.index(max(count_list))])
