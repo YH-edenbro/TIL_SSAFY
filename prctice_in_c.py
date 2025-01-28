084d552ace16034552a4280bd0b078a3ad882e53
YH-edenbro <hyun96227@gmail.com>
2025-01-28 16:01:50 +0900

    임시

--- prctice_in_c.py
+++ prctice_in_c.py
@@ -1,11 +1,12 @@
 a, b, v = map(int, input().split())  #낮에는 a미터, 밤에는 -b미터, 나무막대v
 
 start = 0 # 달팽이의 시작점
-day  = 0
+day = 0
 
 while start >= v :
     start = start + a
     start = start - b
     day = day + 1
+    continue
 
 print(day)
