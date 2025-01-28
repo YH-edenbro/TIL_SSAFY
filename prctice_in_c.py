2e781140cfd365c54abbe51178373ce3df917339
YH-edenbro <hyun96227@gmail.com>
2025-01-28 13:52:38 +0900

    달팽이문제풀기

--- prctice_in_c.py
+++ prctice_in_c.py
@@ -1,15 +1,11 @@
-n = int(input())
-a = []
+a, b, v = map(int, input().split())  #낮에는 a미터, 밤에는 -b미터, 나무막대v
 
-for i in range(n):
-    a.append('*' + ('*' * 2 * i))
+start = 0 # 달팽이의 시작점
+day  = 0
 
-while True:
-    for item in a:
-        print(item.center(2*n))
-    for item2 in a[-2::-1]:
-        print(item2.center(2*n))
-    break
+while start >= v :
+    start = start + a
+    start = start - b
+    day = day + 1
 
-
-
+print(day)
