fa7e5dccc698dac42180e42b916feca1217c8bc9
YH-edenbro <hyun96227@gmail.com>
2025-02-02 18:21:15 +0900

    더하기사이클풀기

--- prctice_in_c.py
+++ prctice_in_c.py
@@ -1,18 +1,28 @@
 import sys
-from collections import deque
 
 n = int(sys.stdin.readline())
-card = [] #카드목록
+a = 0 # 사이클 수
+str_n = str(n)
+new_n = "0"
 
-#카드를 순서에 맞춰 번호 매기기
-for i in range(1,n+1):
-    card.append(i)
 
-card = deque(card)
+while int(new_n) != n:
+    if 1 < n < 10 :
+        str_n = "0" + str_n
+        sum_n = int(str_n[0]) + int(str_n[1])
+        new_n = str_n[-1] + (str(sum_n))[-1]
+        str_n = new_n
+        a = a + 1
+    elif n > 10 :
+        sum_n = int(str_n[0]) + int(str_n[1])
+        new_n = str_n[-1] + (str(sum_n))[-1]
+        str_n = new_n
+        a = a + 1
+    elif n == 0:
+        a = 1
+        break
+    elif n == 1:
+        a = 60
+        break
 
-while len(card) > 1:
-    card.popleft()
-    card.append(card.popleft())
-    
-
-print(*card)
+print(a)
