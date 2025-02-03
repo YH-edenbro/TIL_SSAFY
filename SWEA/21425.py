24c9dfa31ab7e3b1286cd6bf6053e6d08bdf578c
YH-edenbro <hyun96227@gmail.com>
2025-02-04 08:43:48 +0900

    SWEA한문제

--- SWEA/21425.py
+++ SWEA/21425.py
@@ -4,6 +4,8 @@ for i in range(1,T+1):
     m = 0 # 연산 횟수
     a, b, n = map(int, input().split())
    
+
+#  a 와 b 모두 성장해야 하기 때문에 작은 수에 수를 더해줘야 연산 횟수를 줄일 수 있음.
     while True:
         if a > b:
             b += a
