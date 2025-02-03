b15b654497c55b293c937830267d4fbe9883349c
YH-edenbro <hyun96227@gmail.com>
2025-02-04 08:55:44 +0900

    문제풀다만

--- prctice_in_c.py
+++ prctice_in_c.py
@@ -1,19 +1,10 @@
 T = int(input())
 
-for i in range(1,T+1):
-    m = 0 # 연산 횟수
-    a, b, n = map(int, input().split())
-   
-    while True:
-        if a > b:
-            b += a
-            m += 1
-            if b > n:
-                break
-        else:
-            a += b
-            m += 1
-            if a > n:
-                break
+for test_case in range(1, T+1):
+    profit = 0
+    n = int(input())
+    a = list(map(int, input().split()))
 
-    print(m)
+    if a.index(max(a)) == 0:
+        print(f"#{test_case} {profit}")
+    
